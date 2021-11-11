5552e9390350468cd2326e32b0465d003facfde9
jannaesteban <esteban@noseryoung.com>
2021-11-11 16:02:08 +0100

    <ALL-FEAT> implemented an endpoint to get an user by his id

--- src/main/java/com/example/demo/domain/appUser/UserController.java
+++ src/main/java/com/example/demo/domain/appUser/UserController.java
@@ -29,11 +29,18 @@ private final UserService userService;
         return new ResponseEntity<Collection<User>>(userService.findAll(), HttpStatus.OK);
     }
 
+    @GetMapping("/users/{id}")
+    public ResponseEntity<Optional<User>> getUserById(@PathVariable UUID id) throws InstanceNotFoundException {
+        return ResponseEntity.ok().body(userService.findById(id));
+    }
 
     @PutMapping("/user/{id}")
     @PreAuthorize("hasAuthority('ALL_PRIVILEGES')")
     public ResponseEntity<User> editUserById(@RequestBody User editedUser, @PathVariable UUID id) throws InstanceNotFoundException {
         return ResponseEntity.ok().body(userService.editUserInformationById(editedUser, id));
     }
+
+
+
 
 }
